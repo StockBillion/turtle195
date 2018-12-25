b52dd71fc07a333f141d3b1f0e5fb85e5b3cb7b0
朱继玉 <zhuhz82@126.com>
2018-12-25 08:11:42 +0800

    display multi stocks.

--- ttindex.py
+++ ttindex.py
@@ -1,25 +1,18 @@
 #!/usr/bin/env python
 #-*- coding: utf8 -*-
-import argparse, numpy as np, pandas as pd
+import argparse, numpy as np, pandas as pd, math
 import matplotlib.pyplot as plt, mpl_finance as mpf
 # from enum import Enum
 from stockdata import StockDataSet, parse_stock_data
 from account import StockAccount
 
 
-index_code = '000300.sh'
+# index_code = '000300.sh'
+index_codes = ['000001.sh', '000300.sh', '000905.sh', '399673.sz']
 stock_codes = ['601398.sh', '601988.sh', '601628.sh', '600028.sh', '600036.sh', '601318.sh', 
 	'601328.sh', '600000.sh', '601998.sh', '601166.sh', '600030.sh', '600016.sh', 
 	'600519.sh', '600019.sh', '600050.sh', '600104.sh', '601006.sh', '600018.sh', 
 	'000858.sz', '601111.sh', '000002.sz', '600900.sh', '601601.sh', '601991.sh' ]
-
-# # https://tushare.pro/
-# ts.set_token("e2a71ab976c499825f6f48186f24700f70e0f13af933e2f508684cc0")
-# pro = ts.pro_api()
-
-# df = pro.index_weight(index_code='399300.SZ', start_date='20180901', end_date='20180930')
-# print(df)
-
 
 
 class MovingAverage:
@@ -70,10 +63,6 @@ class MovingAverage:
         return self.ma_indexs[n]
 
 
-# class StrongIndex:
-#     '强弱指标'
-#     def __init__(self)
-
 class TurTleIndex:
     '海龟指标'
 
@@ -107,17 +96,18 @@ class TurTleIndex:
         self.wave[short_period] = wavema.moving_average(short_period)
 
         self.trade(long_period, short_period, append_multiple, loss_multiple)
-        self.strong_index(5)
-        self.strong_index(20)
-        self.strong_index(60)
+        # self.strong_index(5)
+        # self.strong_index(20)
+        self.strong_index(120)
 
         self.data['lh_price'] = self.high_prices[10]
-        # self.data['lh_locat'] = self.high_locats[10]
         self.data['ll_price'] = self.low_prices[10]
-        # self.data['ll_locat'] = self.low_locats[10]
 
-        print( pd.DataFrame(self.strong)[220: 250] )
-        print( pd.DataFrame(self.data)[220: 250] )
+        # print( pd.DataFrame(self.strong)[220: 250] )
+        # print( pd.DataFrame(self.data)[220: 250] )
+
+        # self.data['lh_locat'] = self.high_locats[10]
+        # self.data['ll_locat'] = self.low_locats[10]
 
         # print( pd.DataFrame(self.wave)[55: 85] )
         # print( pd.DataFrame(self.high_prices)[55: 85] )
@@ -152,13 +142,13 @@ class TurTleIndex:
 
         self._highest_price(high, period)
         self._lowest_price (low , period)
-        self._highest_price(high, period*3)
-        self._lowest_price (low , period*3)
-
         HP1 = self.high_prices[period]
         LP1 = self.low_prices [period]
-        HP3 = self.high_prices[period*3]
-        LP3 = self.low_prices [period*3]
+
+        # self._highest_price(high, period*3)
+        # self._lowest_price (low , period*3)
+        # HP3 = self.high_prices[period*3]
+        # LP3 = self.low_prices [period*3]
 
         # HP = self.high_prices[period]
         # LP = self.low_prices [period]
@@ -166,22 +156,15 @@ class TurTleIndex:
         # LL = self.low_locats [period]
 
         _strong.append(0)
-        for i in range(1, len(high)):
-            s = (HP1[i] - LP3[i])/LP3[i] + (LP1[i] - HP3[i])/HP3[i]
-            # s = (close[i] - LP3[i])/LP3[i]
+        for i in range(1, len(close)):
+            s = (close[i] - LP1[i])/LP1[i]
+            # s = (HP1[i] - LP3[i])/LP3[i] + (LP1[i] - HP3[i])/HP3[i]
             # s = (high[i] - LP[i])/LP[i] + (low[i] - HP[i])/HP[i]
             # s = (close[i] - LP[i]) / (i - LL[i]) + (close[i] - HP[i]) / (i - HL[i])
             _strong.append(s*100)
 
         self.strong[period] = _strong
 
-        # self._highest_price(self.data['high'], 40)
-        # self._highest_price(self.data['high'], 120)
-        # self._lowest_price (self.data['low' ], 40)
-        # self._lowest_price (self.data['low' ], 120)
-
-        # strong10 = []
-        # strong10 
 
     def trade(self, long_period, short_period, append_multiple, loss_multiple):
         state = []
@@ -343,6 +326,94 @@ class TurTleIndex:
         self.low_locats[n] = ls
 
 
+def log_list(data_list, base0 = 1):
+    _b = math.log(base0)
+    for i in range(0, len(data_list)):
+        # data_list[i] = math.log(data_list[i]) - _b
+        date, open, high, low, close = data_list[i]
+        open = math.log(open) - _b
+        high = math.log(high) - _b
+        low = math.log(low) - _b
+        close = math.log(close) - _b
+        data_list[i] = (date, open, high, low, close)
+    return data_list
+
+
+if __name__ == "__main__":
+    dataset = StockDataSet()
+    startdate = '20170101'
+    enddate = '20190101'
+
+    long_cycle = 55
+    short_cycle= 20
+    stock_datas = {}
+
+    parser = argparse.ArgumentParser(description="show example")
+    parser.add_argument("-s", "--start_date", help="start date")
+    parser.add_argument("-e", "--end_date", help="end date")
+
+    ARGS = parser.parse_args()
+    if ARGS.start_date:
+        startdate = ARGS.start_date
+    if ARGS.end_date:
+        enddate = ARGS.end_date
+
+
+    fig,[ax1,ax2] = plt.subplots(2, 1, sharex=True)
+    fig.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95)
+    # plt.subplots_adjust(left=0.2, bottom=0.2, right=0.8, top=0.8，hspace=0.2, wspace=0.3)
+
+    ax1.xaxis_date()
+    ax1.set_title('indexs')
+    ax1.set_ylabel("price")
+    ax2.set_ylabel('Volume')
+
+    plt.xticks(rotation=45)
+    plt.yticks()
+    plt.xlabel("date")
+
+
+    for code in index_codes:
+        index_data = dataset.load(code, startdate, enddate, 'index', 'daily')
+        dates, data_list, ave_price, volumes = parse_stock_data(index_data)
+        turtle = TurTleIndex(data_list, long_cycle, short_cycle, 1, 2)
+
+        data_list = log_list(data_list, turtle.low_prices[120][len(data_list)-1])
+        mpf.candlestick_ohlc(ax1, data_list, width=1.5, colorup='red', colordown='green')
+        ax2.plot(dates, turtle.strong[120], lw=2, label='wave')
+
+    # ax1.plot(dates, h55, color='y', lw=2, label='high (long_cycle)')
+    # ax1.plot(dates, l20, color='b', lw=2, label='low (short_cycle)')
+    # ax1.plot(dates, avma240, color='g', lw=2, label='MA (240)')
+    # ax1.plot(dates, market_values, color='r', lw=2, label='MV')
+    # ax2.bar(dates, volumes, width=0.75)
+    # ax2.plot(dates, stock_volumes, color='r', lw=2, label='volumes')
+
+    # ax2.plot(dates, turtle.strong[5], color='r', lw=2, label='wave')
+    # ax2.plot(dates, turtle.strong[20], color='g', lw=2, label='wave')
+    # ax2.plot(dates, turtle.strong[60], color='b', lw=2, label='wave')
+    
+    plt.grid()
+    # plt.savefig("./images/turtle2055.png")
+    plt.show()
+
+
+## 准备数据
+    # index_data = dataset.load(index_code, startdate, enddate, 'index', 'daily')
+    # for code in stock_codes:
+    #     stock_datas[code] = dataset.load(code, startdate, enddate, 'stock', 'daily')
+
+# ## 计算指标
+    # calc_ttindex(index_data, long_cycle, short_cycle, 1, 2)
+#     for code in stock_codes:
+# 		calc_ttindex(stock_datas[code], long_cycle, short_cycle)
+
+
+    # turtle = TurTleIndex(data_list, long_cycle, short_cycle, 1, 2)
+    # h55 = turtle.high_prices[55]
+    # l20 = turtle.low_prices[20]
+    # strong = turtle.strong[120]
+
 ## 计算指标
 # def calc_ttindex(data_frame, long_cycle, short_cycle, append_multiple, loss_multiple):
 #     dates, data_list, ave_price, volumes = parse_stock_data(data_frame)
@@ -363,77 +434,6 @@ class TurTleIndex:
     # print(pf.DataFrame(turtle.high_locats))
     # print(turtle.low_prices[short_cycle])
     # print(turtle.low_locats[short_cycle])
-
-
-if __name__ == "__main__":
-    dataset = StockDataSet()
-    startdate = '20150101'
-    enddate = '20190101'
-
-    long_cycle = 55
-    short_cycle= 20
-    stock_datas = {}
-
-    parser = argparse.ArgumentParser(description="show example")
-    parser.add_argument("-s", "--start_date", help="start date")
-    parser.add_argument("-e", "--end_date", help="end date")
-
-    ARGS = parser.parse_args()
-    if ARGS.start_date:
-        startdate = ARGS.start_date
-    if ARGS.end_date:
-        enddate = ARGS.end_date
-
-## 准备数据
-    index_data = dataset.load(index_code, startdate, enddate, 'index', 'daily')
-    # for code in stock_codes:
-    #     stock_datas[code] = dataset.load(code, startdate, enddate, 'stock', 'daily')
-
-# ## 计算指标
-    # calc_ttindex(index_data, long_cycle, short_cycle, 1, 2)
-#     for code in stock_codes:
-# 		calc_ttindex(stock_datas[code], long_cycle, short_cycle)
-
-
-    dates, data_list, ave_price, volumes = parse_stock_data(index_data)
-    turtle = TurTleIndex(data_list, long_cycle, short_cycle, 1, 2)
-
-    h55 = turtle.high_prices[55]
-    l20 = turtle.low_prices[20]
-    # strong = turtle.strong[120]
-
-    fig,[ax1,ax2] = plt.subplots(2, 1, sharex=True)
-    fig.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95)
-    # plt.subplots_adjust(left=0.2, bottom=0.2, right=0.8, top=0.8，hspace=0.2, wspace=0.3)
-
-    ax1.xaxis_date()
-    ax1.set_title(index_code)
-    ax1.set_ylabel("price")
-
-    plt.xticks(rotation=45)
-    plt.yticks()
-    plt.xlabel("date")
-
-
-    mpf.candlestick_ohlc(ax1, data_list, width=1.5, colorup='r', colordown='green')
-    ax1.plot(dates, h55, color='y', lw=2, label='high (long_cycle)')
-    ax1.plot(dates, l20, color='b', lw=2, label='low (short_cycle)')
-
-    # ax1.plot(dates, avma240, color='g', lw=2, label='MA (240)')
-    # ax1.plot(dates, market_values, color='r', lw=2, label='MV')
-
-    # ax2.bar(dates, volumes, width=0.75)
-
-    # ax2.plot(dates, stock_volumes, color='r', lw=2, label='volumes')
-
-    ax2.plot(dates, turtle.strong[5], color='r', lw=2, label='wave')
-    ax2.plot(dates, turtle.strong[20], color='g', lw=2, label='wave')
-    ax2.plot(dates, turtle.strong[60], color='b', lw=2, label='wave')
-    ax2.set_ylabel('Volume')
-
-    plt.grid()
-    # plt.savefig("./images/turtle2055.png")
-    plt.show()
 
 
     # TTState = Enum('TTState', ('NONE', 'FIRST', 'SECOND', 'THIRD', 'FOURTH', 'STOP_LOSS', 'STOP_PROFIT'))
@@ -457,6 +457,14 @@ if __name__ == "__main__":
     #     h55 = turtle.high_indexs[long_cycle]
     #     l20 = turtle.low_indexs[short_cycle]
 
+
+        # self._highest_price(self.data['high'], 40)
+        # self._highest_price(self.data['high'], 120)
+        # self._lowest_price (self.data['low' ], 40)
+        # self._lowest_price (self.data['low' ], 120)
+
+        # strong10 = []
+        # strong10 
 
 
             # if high[i] > lh[i] and not long_count:
@@ -505,3 +513,14 @@ if __name__ == "__main__":
 
 # from turtle import TurTleIndex
 
+
+# # https://tushare.pro/
+# ts.set_token("e2a71ab976c499825f6f48186f24700f70e0f13af933e2f508684cc0")
+# pro = ts.pro_api()
+
+# df = pro.index_weight(index_code='399300.SZ', start_date='20180901', end_date='20180930')
+# print(df)
+
+# class StrongIndex:
+#     '强弱指标'
+#     def __init__(self)
